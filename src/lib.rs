9f637d27fe0976f3ade04322c9eca3e173cee928
Tibor Benke <ihrwein@gmail.com>
2015-10-12 22:01:57 +0200

    tokenizer: support Bracket tokens
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #[derive(Debug, Eq, PartialEq)]
 pub enum Token {
     Brace(Vec<Token>),
+    Bracket(Vec<Token>),
     Literal(String),
     Float(String),
     Int(String),
@@ -21,6 +22,7 @@ token_seq -> Vec<Token> = token ** space
 
 token -> Token
   = brace_token
+  / bracket_token
   / hex_token
   / ipv4_token
   / mac_token
@@ -34,6 +36,9 @@ literal_token -> Token
 
 brace_token -> Token
     = "{" tokens:token_seq "}" { Token::Brace(tokens) }
+
+bracket_token -> Token
+    = "[" tokens:token_seq "]" { Token::Bracket(tokens) }
 
 float_token -> Token
     = [-+]? [0-9]* "."? [0-9]+ ([eE][-+]?[0-9]+)? { Token::Float(match_str.to_string()) }
@@ -203,6 +208,21 @@ mod tests {
       let result = tokenizer::message(message);
       println!("{:?}", &result);
       let token = result.ok().expect("Failed to parse a valid message when it contains braces");
+      assert_eq!(&expected, &token);
+    }
+
+    #[test]
+    fn test_given_tokenizer_when_it_parses_tokens_in_brackets_then_we_get_the_expected_composite_token() {
+      let message = "[42 0x12]";
+      let expected = vec![
+        Token::Bracket(vec![
+            Token::Int("42".to_string()),
+            Token::HexString("0x12".to_string()),
+        ])
+      ];
+      let result = tokenizer::message(message);
+      println!("{:?}", &result);
+      let token = result.ok().expect("Failed to parse a valid message when it contains brackets");
       assert_eq!(&expected, &token);
     }
 }
