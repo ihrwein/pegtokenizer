7b18f1d9d5585255790ca488a242773a1064aaee
Tibor Benke <ihrwein@gmail.com>
2015-10-15 06:33:33 +0200

    main: add a main() function so this crate produces a binary as well
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use std::io::prelude::*;
+
+extern crate pegtokenizer;
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(line) => {
+                let tokens = pegtokenizer::tokenize(&line);
+                println!("{:?}", tokens);
+            },
+            Err(error) => {
+                println!("{}", error);
+                break
+            }
+        }
+    }
+}
